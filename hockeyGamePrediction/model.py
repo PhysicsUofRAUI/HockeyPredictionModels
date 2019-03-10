fb7eda30f99be3d460f4839f2b787732894b1947
PhysicsUofRAUI <kodyrogers21@gmail.com>
2019-03-09 18:43:18 -0600

    created a few models and added the data

--- hockeyGamePrediction/model.py
+++ hockeyGamePrediction/model.py
@@ -1,23 +1,129 @@
-import utilityFunctions.py
-
+import numpy
+import pandas
 from keras.models import Sequential
 from keras.layers import Dense
-import numpy
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
 
-seasonThirteenFourteen = createBaseDataList(teamsList, "2013-2014Season.txt")
-seasonFourteenFifteen = createBaseDataList(teamsList, "2014-2015Season.txt")
-seasonFifteenSixteen = createBaseDataList(teamsList, "2015-2016Season.txt")
-seasonSixteenSeventeen = createBaseDataList(teamsList, "2016-2017Season.txt")
-seasonSeventeenEightteen = createBaseDataList(teamsList, "2017-2018Season.txt")
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
 
-ThirteenFourteenRecord = returnCurrentRecordBetweenTeams(seasonThirteenFourteen)
-FourteenFifteenRecord = returnCurrentRecordBetweenTeams(seasonFourteenFifteen)
-FifteenSixteenRecord = returnCurrentRecordBetweenTeams(seasonFifteenSixteen)
-SixteenSeventeenRecord = returnCurrentRecordBetweenTeams(seasonSixteenSeventeen)
-SeventeenEightteenRecord = returnCurrentRecordBetweenTeams(seasonSeventeenEightteen)
+# load dataset
+dataframe = pandas.read_csv("iris.csv", header=None)
+dataset = dataframe.values
+X = dataset[:,0:38].astype(float)
+Y = dataset[:,38]
 
-addRecordsBetweenTeamsToData(ThirteenFourteenRecord, seasonThirteenFourteen)
-addRecordsBetweenTeamsToData(FourteenFifteenRecord, seasonFourteenFifteen)
-addRecordsBetweenTeamsToData(FifteenSixteenRecord, seasonFifteenSixteen)
-addRecordsBetweenTeamsToData(SixteenSeventeenRecord, seasonSixteenSeventeen)
-addRecordsBetweenTeamsToData(SeventeenEightteenRecord, seasonSeventeenEightteen)
+# encode class values as integers
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+# convert integers to dummy variables (i.e. one hot encoded)
+dummy_y = np_utils.to_categorical(encoded_Y)
+
+# define baseline model
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(8, input_dim=38, activation='relu'))
+	model.add(Dense(3, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=0)
+
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+
+# define baseline model
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(8, input_dim=38, activation='relu'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=0)
+
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("4 layers with softmax, 200 epochs, batch_size 5")
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100)
+
+# define baseline model
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(8, input_dim=38, activation='relu'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs=1000, batch_size=5, verbose=0)
+
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("4 layers with softmax, 1000 epochs, batch_size 5")
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100)
+
+# define baseline model
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(8, input_dim=38, activation='relu'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs=2000, batch_size=5, verbose=0)
+
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("4 layers with softmax, 2000 epochs, batch_size 5")
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100)
+
+# define baseline model
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(8, input_dim=38, activation='relu'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	model.add(Dense(50, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs=5000, batch_size=5, verbose=0)
+
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("4 layers with softmax, 5000 epochs, batch_size 5")
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100)
